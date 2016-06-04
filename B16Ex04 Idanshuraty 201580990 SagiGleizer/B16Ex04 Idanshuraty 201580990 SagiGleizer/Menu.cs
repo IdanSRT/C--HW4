9d9f95629d35062bd6c7d05d623c029b14fb711e
Idan.Shuraty <idanshuraty89@gmail.com>
2016-06-04 15:46:46 +0300

    Startind design

---
+++ B16Ex04 Idanshuraty 201580990 SagiGleizer/B16Ex04 Idanshuraty 201580990 SagiGleizer/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B16Ex04_Idanshuraty_201580990_SagiGleizer
+{
+    class Menu : IClickable
+    {
+        private string m_Title;
+        private List<Menu> m_MenuItems;
+        
+        public string Title
+        {
+            get { return m_Title; }
+            set { m_Title = value; }
+        }
+        public List<Menu> MenuItems
+        {
+            get { return m_MenuItems; }
+            set { m_MenuItems = value; }
+        }
+
+        public void Onlick()
+        {
+            //Do something
+        }
+    }
+}
